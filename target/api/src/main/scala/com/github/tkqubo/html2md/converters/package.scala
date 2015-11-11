9fc019f15a64aaf6355ec75f98d30c8121c2db82
tkqubo <tk.qubo@gmail.com>
2015-11-11 18:16:53 +0900

    docs: create docs

--- target/api/src/main/scala/com/github/tkqubo/html2md/converters/package.scala
+++
@@ -1,6 +0,0 @@
-package com.github.tkqubo.html2md
-
-/**
-  * Defines various markdown conversion rules as [[com.github.tkqubo.html2md.converters.MarkdownConverter]] implementations
-  */
-package object converters
